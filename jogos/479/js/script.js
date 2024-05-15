97f581b8856939a04eddc15f8bcc6f4c812b5577
Augusto <argus@MacBook-Air-de-Augusto.local>
2024-05-15 11:41:39 -0300

    custo

--- jogos/479/js/script.js
+++ jogos/479/js/script.js
@@ -1,25 +1,35 @@
 // Animation
 var pintura2=document.querySelector(".pintura2");
-const animation = anime({
-    targets: pintura2,
-    opacity: 1,
+var pintura3=document.querySelector(".pintura3");
+
+  var tl = anime.timeline({
     duration: 4000,
     autoplay: false,
   });
   
-  const section2 = document.querySelector(".section-2");
+  tl
+  .add({
+    targets: pintura2,
+    translateX: 200,
+  })
+  .add({
+    targets: pintura3,
+    translateX: 300,
+  }, '-=100')
+
   
   // Animate on scroll
   const animateOnScroll = function () {
     const scrollPercent = window.pageYOffset;
-    console.log(scrollPercent,window.pageYOffset,window.innerHeight);
+    //console.log((scrollPercent + 0) / 100);
     return (scrollPercent + 0) / 100;
   };
   
   // Scroll listener
   window.onscroll = function () {
     //animation.seek(animateOnScroll(pintura2, 1000, 200) * animation.duration);
-    animation.seek((animateOnScroll() / 100) * animation.duration);
+    //console.log((animateOnScroll() / 100) * animation.duration)
+    tl.seek((animateOnScroll() / 100) * tl.duration);
     
   };
   
