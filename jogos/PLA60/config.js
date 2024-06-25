e21385b9a3092d500829b666808bf558cf583807
Augusto Flavio Barbosa <augusto.barbosa@saedigital.com.br>
2024-06-25 16:38:10 -0300

    app teste9

---
+++ jogos/PLA60/config.js
@@ -0,0 +1,45 @@
+window.JSONConfig = {
+    "name": "Editor de quadrinhos",
+    "skin" : "opaqueBlue",
+    "questions" : [
+        {
+            "id": "intro",
+            "label" : "Introdução",
+            "howToDo": "Selecione os quadrinhos abaixo para editar sua tirinha.",
+            "buttons": {
+                "confirm" : false,
+                "reset"   : false,
+                "clear"   : false,
+                "back"    : false,
+                "readHistory": true,
+            }
+        },
+
+        {
+            "id": "editor",
+            "label" : "Editor de quadrinhos",
+            "howToDo": "",
+            "buttons": {
+                "confirm" : false,
+                "reset"   : false,
+                "clear"   : true,
+                "back"    : true,
+                "readHistory": true,
+            }
+        },
+        {
+            "id": "historia",
+            "label": "Editor de quadrinhos",
+            "howToDo": "",
+            "buttons": {
+                "confirm" : false,
+                "reset"   : false,
+                "clear"   : false,
+                "back"    : true,
+                "readHistory": false,
+            }
+        }
+
+
+    ]
+}
